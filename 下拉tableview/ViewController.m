cb7a92b80f921aea41409ae8ff580bf69ea35352
582324909 <582324909@qq.com>
2016-12-09 17:44:45 +0800

    The drop-down headview amplification
    The drop-down headview amplification

--- 下拉tableview/ViewController.m
+++ 下拉tableview/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "HFStretchableTableHeaderView.h"
+
+#define StretchHeaderHeight 200
+
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableview;
+@property (nonatomic,strong)HFStretchableTableHeaderView *stretchHeaderView;
 
 @end
 
@@ -17,8 +24,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    [self initStretchHeader];
+    
 }
 
+- (void)initStretchHeader
+{
+    //背景
+    UIImageView *bgImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, StretchHeaderHeight)];
+    bgImageView.contentMode = UIViewContentModeScaleAspectFill;
+    bgImageView.clipsToBounds = YES;
+    bgImageView.image = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"001" ofType:@"jpg"]];
+    self.stretchHeaderView = [HFStretchableTableHeaderView new];
+    [self.stretchHeaderView stretchHeaderForTableView:self.tableview withView:bgImageView subViews:nil];
+    
+}
+
+#pragma mark - table delegate
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 40;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellId = @"reuseCell";
+    UITableViewCell *cell = [self.tableview dequeueReusableCellWithIdentifier:cellId ];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    cell.textLabel.textAlignment = NSTextAlignmentCenter;
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self.tableview deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - stretchableTable delegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self.stretchHeaderView scrollViewDidScroll:scrollView];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [self.stretchHeaderView resizeView];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
